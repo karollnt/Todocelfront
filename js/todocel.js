289738dbb31b780b80af93c49093a8faad25c51c
rhonaldomaster <rhonalf.martinez@koombea.com>
2016-11-01 16:02:36 -0500

    codigo para manipulacion del carrito

--- js/todocel.js
+++ js/todocel.js
@@ -1,7 +1,47 @@
-var todocel = ()(function () {
+;
+var todocel = (function () {
+  var config = {
+    $document: $(document),
+    backend: 'http://localhost/TodoCel'
+  }
+
+  var init = function () {
+    todocel.navLinks.init();
+    todocel.cartHandler.init();
+  };
+
+  return {
+    config: config,
+    init: init
+  }
+})();
+
+todocel.navLinks = (function () {
+  var init = function () {
+    $('.main-nav a').on('click',function (ev) {
+      var element = ev.currentTarget;
+      ev.preventDefault();
+      loadPage(element);
+    });
+  };
+
+  var loadPage = function (element) {
+    var url = element.href;
+    mainView.router.loadPage(url+"?"+(Math.floor((Math.random() * 1000) + 1)));
+    if (url.indexOf('shop') > -1) {
+      todocel.productos.listarProductos();
+    }
+  }
+
+  return {
+    init: init
+  }
+})();
+
+todocel.productos = (function () {
   function listarProductos() {
     var ajx = $.ajax({
-      url: '',
+      url: todocel.config.backend+'/productos/listarProductos',
       method: 'post',
       data: {}
     });
@@ -9,7 +49,7 @@ var todocel = ()(function () {
       var productos = data.productos;
       var html = '';
       $.each(productos,function (index,producto) {
-        html += renderProduct();
+        html += renderProduct(producto);
       });
       $('.js-shop-items').html(html);
     });
@@ -18,21 +58,9 @@ var todocel = ()(function () {
   function renderProduct(product) {
     var html = '';
     if (product) {
-      html = '<li>'
-        +'<div class="shop_thumb"><a href="shop-item.html"><img src="images/shop_thumb1.jpg" alt="" title="" /></a></div>'
-        +'<div class="shop_item_details">'
-          +'<h4><a href="shop-item.html">'+product.nombre+'</a></h4>'
-          +'<div class="shop_item_price">'+product.precio+'</div>'
-          +'<div class="item_qnty_shop">'
-            +'<form id="myform" method="POST" action="#">'
-              +'<input type="hidden" name="id" value="'+product.id+'" />'
-              +'<input type="text" name="quantity'+product.id+'" value="1" class="qntyshop" />'
-            +'</form>'
-          +'</div>'
-          +'<a href="#" data-panel="left" id="addtocart" class="open-panel">Agregar al carrito</a>'
-          +'<a href="#" data-popup=".popup-social" class="open-popup shopfav"><img src="images/icons/yellow/love.png" alt="" title="" /></a>'
-        +'</div>'
-      +'</li>';
+      var source = $("#shopProductItem").html();
+      template = Handlebars.compile(source);
+      html = '<li>'+ template(product) +'</li>';
     }
     return html;
   }
@@ -40,6 +68,75 @@ var todocel = ()(function () {
   return {
     listarProductos: listarProductos
   }
-});
+})();
 
-todocel.listarProductos();
+todocel.cartHandler = (function () {
+  var storage = window.localStorage;
+  var cartData = {items: []};
+
+  var init = function () {
+    var data = storage.getItem('todocelCart');
+    if (data) {
+      cartData = JSON.parse(data);
+    }
+    todocel.config.$document.on('submit','.js-cart-element-form',addToCart);
+    todocel.config.$document.on('click','.js-cart-remove-item',removeFormCart);
+  };
+
+  var addToCart = function (ev) {
+    ev.preventDefault();
+    var form = ev.currentTarget;
+    var item = {
+      id: form.querySelector('.js-cart-element-id').value,
+      quantity: form.querySelector('.js-cart-element-quantity').value,
+      number: cartData.items.length,
+      name: form.querySelector('.js-cart-element-name').value,
+      price: form.querySelector('.js-cart-element-price').value
+    };
+    addItem(item);
+  };
+
+  var addItem = function (item) {
+    cartData.items.push(item);
+    saveCart();
+  };
+
+  var removeFormCart = function (ev) {
+    var item;
+    ev.preventDefault();
+    var elem = ev.currentTarget;
+    var dataSize = cartData.items.length;
+    for (var i = 0; i < dataSize; i++) {
+      if (cartData.items[i].id == elem.dataset.id) {
+        item = cartData.items[i];
+      }
+    }
+    if(item) removeItem(item);
+  }
+
+  var removeItem = function (item) {
+    var dataSize = cartData.items.length;
+    for (var i = 0; i < dataSize; i++) {
+      if (cartData.items[i].id == item.id) {
+        cartData.items.splice(i,1);
+      }
+    }
+    saveCart();
+  };
+
+  var saveCart = function () {
+    var strc = JSON.stringify(cartData);
+    storage.setItem('todocelCart',strc);
+  };
+
+  var emptyCart = function () {
+    cartData = {items: []};
+    storage.removeItem('todocelCart');
+  }
+
+  return {
+    init: init
+  }
+})();
+
+todocel.init();
