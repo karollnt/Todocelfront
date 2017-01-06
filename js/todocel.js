2c373362667d060251226fc33379d18198cc074b
rhonaldomaster <rhonalf.martinez@koombea.com>
2017-01-06 18:10:58 -0500

    actualizacion para manejo de sesion

--- js/todocel.js
+++ js/todocel.js
@@ -9,8 +9,10 @@ var todocel = (function () {
     todocel.navLinks.init();
     todocel.cartHandler.init();
     todocel.productos.init();
-
-    todocel.config.$document.on('click','.js-open-cart',todocel.cartHandler.init);
+    todocel.users.init();
+    todocel.config.$document
+      .on('click','.js-open-cart',todocel.cartHandler.init)
+      .on('submit','.js-login-form',todocel.users.login);
   };
 
   return {
@@ -20,56 +22,86 @@ var todocel = (function () {
 })();
 
 todocel.utils = (function () {
-  var fillMonthSelect = function (id) {
-    var str = "";
+  var fillMonthSelect = function (selector) {
+    var str = '';
     for (var i = 1; i <= 12; i++) {
-      str += "<option value='"+i+"'>"+i+"</option>";
+      str += '<option value="'+i+'">'+i+'</option>';
     }
-    $(id).html(str);
+    document.querySelector(selector).innerHTML = str;
   };
 
-  var fillYearSelect = function (id) {
+  var fillYearSelect = function (selector) {
     var fd = new Date();
     var miny = fd.getFullYear();
     var maxy = miny + 30;
-    var str = "";
+    var str = '';
     for (var i = miny; i < maxy; i++) {
-      str += "<option value='"+i+"'>"+i+"</option>";
+      str += '<option value="'+i+'">'+i+'</option>';
     }
-    $(id).html(str);
+    document.querySelector(selector).innerHTML = str;
+  };
+
+  var validateEmail = function (email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+
+  var formToJSONString = function (form) {
+    var obj = {};
+    var elements = form.querySelectorAll( 'input, select, textarea' );
+    for( var i = 0; i < elements.length; ++i ) {
+      var element = elements[i];
+      var name = element.name;
+      var value = element.value;
+      if( name ) {
+        obj[ name ] = value.trim();
+      }
+    }
+    return JSON.stringify( obj );
   };
 
   return {
     fillMonthSelect: fillMonthSelect,
-    fillYearSelect: fillYearSelect
+    fillYearSelect: fillYearSelect,
+    validateEmail: validateEmail,
+    formToJSONString: formToJSONString
   };
 })();
 
 todocel.navLinks = (function () {
   var init = function () {
-    todocel.config.$document.on('click','.main-nav a, .js-nav-item',function (ev) {
-      var element = ev.currentTarget;
+    $('.js-main-nav a').off('click');
+    todocel.config.$document.on('click','.js-main-nav a',function (ev) {
       ev.preventDefault();
+      var element = ev.currentTarget;
       loadPage(element);
     });
   };
 
   var loadPage = function (element) {
-    var url = element.href;
-    mainView.router.loadPage(url+"?"+(Math.floor((Math.random() * 1000) + 1)));
-    if (url.indexOf('shop') > -1) {
-      todocel.productos.listarProductos();
+    var isSessionStarted = todocel.users.verifyLoggedIn();
+    if (isSessionStarted) {
+      var url = element.dataset.link;
+      if (url) {
+        mainView.router.loadPage(url+"?"+(Math.floor((Math.random() * 1000) + 1)));
+        if (url.indexOf('shop') > -1) {
+          todocel.productos.listarProductos();
+        }
+        if (url.indexOf('index') > -1) {
+          todocel.cartHandler.init();
+        }
+        if (url.indexOf('pasarela') > -1) {
+          setTimeout(function(){
+            mercpagoui.initEvents();
+            todocel.utils.fillMonthSelect('.js-expirationMonth');
+            todocel.utils.fillYearSelect('.js-expirationYear');
+            todocel.payments.init();
+          },1000);
+        }
+      }
     }
-    if (url.indexOf('index') > -1) {
-      todocel.cartHandler.init();
-    }
-    if (url.indexOf('pasarela') > -1) {
-      setTimeout(function(){
-        mercpagoui.initEvents();
-        todocel.utils.fillMonthSelect('.js-expirationMonth');
-        todocel.utils.fillYearSelect('.js-expirationYear');
-        todocel.payments.init();
-      },1000);
+    else {
+      myApp.popup('.popup-login');
     }
   };
 
@@ -176,6 +208,7 @@ todocel.cartHandler = (function () {
     if (data) {
       cartData = JSON.parse(data);
       renderCart();
+      $('.js-cart-items-counter').html(cartData.items.length);
     }
     todocel.config.$document.off('submit','.js-cart-element-form');
     todocel.config.$document.on('submit','.js-cart-element-form',addToCart);
@@ -385,6 +418,73 @@ todocel.payments = (function () {
     init: init,
     sendPayment: sendPayment,
     processPayment: processPayment
+  };
+})();
+
+todocel.users = (function () {
+  var isLoggedIn = false;
+  var $loginElem = $('.js-login-link');
+  var init = function () {
+    isLoggedIn = window.localStorage.getItem('nickname')!=null;
+    if (!isLoggedIn) {
+      myApp.popup('.popup-login');
+    }
+    changeIcons();
+  };
+
+  var verifyLoggedIn = function () {
+    return isLoggedIn;
+  };
+
+  var login = function (ev) {
+    ev.preventDefault();
+    var form = ev.target;
+    var jsonForm = todocel.utils.formToJSONString(form);
+    jsonForm = JSON.parse(jsonForm);
+    if (jsonForm.nickname != '') {
+      jsonForm.clave = md5(jsonForm.clave);
+      var ajx = $.ajax({
+        url: todocel.config.backend+'/sesiones/login',
+        type: 'post',
+        dataType: 'json',
+        data: jsonForm
+      });
+      ajx.done(function (data) {
+        if (data.msg == 'ok') {
+          myApp.closeModal('.popup-login');
+          isLoggedIn = true;
+          window.localStorage.setItem('nickname',jsonForm.nickname);
+          changeIcons();
+        }
+      });
+    }
+  };
+
+  var logout = function (ev) {
+    ev.preventDefault();
+    isLoggedIn = false;
+    window.localStorage.removeItem('nickname');
+    myApp.popup('.popup-login');
+    changeIcons();
+  };
+
+  var changeIcons = function () {
+    if (isLoggedIn) {
+      $loginElem.find('img').prop('src','images/icons/yellow/logout.png');
+      $loginElem.find('span').html('Logout');
+      $loginElem.off('click').on('click',logout);
+    }
+    else {
+      $loginElem.find('img').prop('src','images/icons/yellow/user.png');
+      $loginElem.find('span').html('Login');
+      $loginElem.off('click').on('click',init);
+    }
+  };
+
+  return {
+    init: init,
+    verifyLoggedIn: verifyLoggedIn,
+    login: login
   };
 })();
 
